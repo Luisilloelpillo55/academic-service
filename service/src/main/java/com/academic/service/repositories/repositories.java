f543ad3e02e1755429d5af8e24ccfea18a67b3b0
Luisilloelpillo55 <2023371058@uteq.edu.mx>
2026-06-10 23:05:30 -0600

    Delete service/src/main/java/com/academic/service/repositories/repositories.java

--- service/src/main/java/com/academic/service/repositories/repositories.java
+++
@@ -1,8 +0,0 @@
-package com.academic.service.repositories;
-
-/**
- * Este paquete contiene las interfaces de repositorio (Data Access Layer).
- * Repositorios implementados:
- * - DivisionRepository.java
- * - ProgramaEducativoRepository.java
- */
