db1d85d305578c5d8eefc049a0e82659bc19f0a4
Luisilloelpillo55 <2023371058@uteq.edu.mx>
2026-06-10 23:04:36 -0600

    Delete service/src/main/java/com/academic/service/controllers/controllers.java

--- service/src/main/java/com/academic/service/controllers/controllers.java
+++
@@ -1,7 +0,0 @@
-package com.academic.service.controllers;
-
-/**
- * Este paquete contiene los controladores REST.
- * Controladores implementados:
- * - DivisionController.java - Maneja endpoints para gestionar divisiones
- */
