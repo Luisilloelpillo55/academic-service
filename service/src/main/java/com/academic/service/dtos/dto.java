cca852e570a005a69c62682196e73e6998e83d85
Luisilloelpillo55 <2023371058@uteq.edu.mx>
2026-06-10 23:04:53 -0600

    Delete service/src/main/java/com/academic/service/dtos/dto.java

--- service/src/main/java/com/academic/service/dtos/dto.java
+++
@@ -1,8 +0,0 @@
-package com.academic.service.dtos;
-
-/**
- * Este archivo contiene las clases DTO (Data Transfer Objects).
- * Las clases públicas han sido movidas a sus propios archivos:
- * - DivisionDTO.java
- * - DivisionUpdateDTO.java
- */
